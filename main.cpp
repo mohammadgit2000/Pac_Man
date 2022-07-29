18520f9a847a908637aca1ff1ccd6579d5a819a1
mohammadgit2000 <mohammadgit7299@gmail.com>
2022-07-29 13:37:27 +0100

    just for test T_shape

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 int main()
 {
     Set_Map_Empty();
-    Insert_Line(std::make_pair(0 ,0) ,std::make_pair(5 ,5));
+    Insert_Shape(std::make_pair(0 ,0) ,std::make_pair(2 ,2) , WALL , std::make_pair(1 ,10));
 
     Print_Map();
     
